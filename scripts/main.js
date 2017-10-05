49fef9909664479448c598fd062bfd79e7514ce7
Edward Wong <wong.eddiek@gmail.com>
2017-10-05 13:03:18 -0600

    added interactive main text section

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
 var textBarBox = $('.text-bar-box');
 
 textBarBox.on('click', function(){
+  // add class text-bar-active to clicked element and remove it to sibling elements
   $(this).children().addClass('text-bar-active');
   $(this).siblings().children().removeClass('text-bar-active');
+
+  // grabbing the data attribute of the clicked element
+  var clickedText = $(this).attr('data-text');
+
+  $(`#${clickedText}`).siblings().hide();
+  $(`#${clickedText}`).show();
+
+  $('.btn-main').html(`more on ${clickedText.replace(/-/," ")} &nbsp; <i id="btn-play" class="fa fa-play" aria-hidden="true"></i>`);
 });
